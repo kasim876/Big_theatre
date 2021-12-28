49ce933c0a3a667547fbf3d04d858b28cd5f453b
kasim876 <94377630+kasim876@users.noreply.github.com>
2021-12-28 18:46:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 "use strict"
 
-document.querySelector('.nav__menu').addEventListener("click", navMenu);
-
-function navMenu() {
-	document.querySelector('.nav__menu__open').classList.toggle('change')
-	document.querySelector('.nav__menu__close').classList.toggle('change')
-}
-
+document.querySelector('.nav__menu__btn').addEventListener("click", navMenu);
 document.querySelector('.nav__menu__close').addEventListener("click", navMenu);
 
+let items = document.querySelectorAll('.nav__menu__item');
+for (let item of items) {
+	item.addEventListener("click", navMenu)
+}
+
+function navMenu() {
+	document.querySelector('.nav__menu').classList.toggle('open');
+}
+
+let goTopBtn = document.querySelector('.back_to_top');
+window.addEventListener("scroll", trackScroll);
+
+function trackScroll() {
+	let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+}
