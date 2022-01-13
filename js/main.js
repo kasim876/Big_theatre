4d5adcf8287c4f051335f01744c8b2be2db0db5c
kasim876 <94377630+kasim876@users.noreply.github.com>
2022-01-13 18:46:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,71 @@
 "use strict"
 
-document.querySelector('.nav__menu__btn').addEventListener("click", navMenu);
-document.querySelector('.nav__menu__close').addEventListener("click", navMenu);
+const next = document.querySelector(".arrow1__svg"),
+      prev = document.querySelector(".arrow2__svg"),
+      items = document.querySelectorAll(".nav__menu__item"),
+      goTopBtn = document.querySelector(".back_to_top"),
+      slides = document.querySelectorAll(".slide"),
+      modal = document.querySelector(".modal");
 
-let items = document.querySelectorAll('.nav__menu__item');
-for (let item of items) {
-	item.addEventListener("click", navMenu)
-}
+let index = 0;
 
-function navMenu() {
-	document.querySelector('.nav__menu').classList.toggle('open');
-}
+const navMenu = () => {
+        document.querySelector('.nav__menu').classList.toggle('open');
+        modal.classList.toggle("active");
+};
 
-let goTopBtn = document.querySelector('.back_to_top');
-window.addEventListener("scroll", trackScroll);
-
-function trackScroll() {
+const trackScroll = () => {
 	let scrolled = window.pageYOffset;
     let coords = document.documentElement.clientHeight;
-
-    if (scrolled > coords) {
+    if (scrolled > coords / 2) {
       goTopBtn.classList.add('back_to_top-show');
-    }
-    if (scrolled < coords) {
+    } else if (scrolled < coords / 2) {
       goTopBtn.classList.remove('back_to_top-show');
     }
+};
+
+const returnTop = () => {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -45);
+        setTimeout(returnTop, 0);
+    }
+};
+
+const activeSlide = n => {
+    for (let slide of slides) {
+        slide.classList.remove("active");
+    }
+    slides[n].classList.add("active");
 }
+
+const nextSlide = () => {
+    if (index === slides.length - 1){
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if (index === 0){
+        index = slides.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+}
+
+for (let item of items) {
+	item.addEventListener("click", navMenu);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+goTopBtn.addEventListener("click", returnTop);
+window.addEventListener("scroll", trackScroll);
+document.querySelector(".nav__menu__btn").addEventListener("click", navMenu);
+document.querySelector(".nav__menu__close").addEventListener("click", navMenu);
+modal.addEventListener("click", navMenu);
